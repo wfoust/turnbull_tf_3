f4b3c028a994560360267cbd9aa3250a99959ff8
Bill <foustwl@gmail.com>
2018-02-20 07:11:28 -0600

    Moved /base/ state file to S3.

--- base/base.tf
+++ base/base.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+terraform {
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "wfoust-remote-state-base"
+    key = "terraform.tfstate"
+  }
+}
+
 module "remote_state" {
   source = "/home/bill/PycharmProjects/terraform/turnbull-book-ch4/remote_state"
   prefix = "${var.prefix}"
